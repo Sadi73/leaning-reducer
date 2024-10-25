d27bd9bc5f62009e9c079b149483136b9fec2cbf
Sadi Muhammad Tanzim <saditanzim@gmail.com>
2024-10-25 16:54:32 +0600

    project initialized

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./Components/Header"
+import MainContainer from "./Components/MainContainer"
+import Sidebar from "./Components/Sidebar"
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Sidebar />
+      <MainContainer />
+    </>
+  )
+}
+
+export default App
